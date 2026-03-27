85ac5a73f65742ebd6c00ed1176f655d8c1e8314
Uladzislau Hramyka <awayfromgalaxy@gmail.com>
2026-03-27 12:02:07 +0300

    fix: correct astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
     site: "https://secondthunder.github.io",
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
